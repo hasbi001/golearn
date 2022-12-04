68b02c4ee50c0e1b11284e5064549de7bedd0b20
Hasbi Ali Firdaus <hasbialifirdaus001@gmail.com>
2022-12-04 23:08:42 +0700

    learn data type in golang

---
+++ typedata.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	// (start) type data integer
+	// minimal limit of int start from minus 
+	var age int8 = 10
+	fmt.Println(age);
+
+	// minimal limit of uint start from 0 
+	var tall uint = 10
+	fmt.Println(tall);	
+	// (end) type data integer
+}
