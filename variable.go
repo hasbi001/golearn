b8e03145254970ad94eb8869e96924cc7e03defa
Hasbi Ali Firdaus <hasbialifirdaus001@gmail.com>
2022-12-09 17:39:02 +0700

    learning variable

---
+++ variable.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string
+
+	name = "Alsky"
+	fmt.Println(name);
+
+	var umur int8 = 15
+	fmt.Println(umur);
+
+	alamat := "jakarta"
+	fmt.Println(alamat);
+
+	// int not allowed 0 in first number
+	var nohp uint64 = 	812334343
+	fmt.Println(nohp);			
+}
