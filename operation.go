4255e402dee7c522450f38262b00a35b018ce901
Hasbi Ali Firdaus <hasbialifirdaus001@gmail.com>
2022-12-31 00:35:39 +0700

    operasi matematika

---
+++ operation.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	// 1. Matematika + - / % 
+	var no1 int32;
+
+	no1 = 10;
+	no1 += 4; 
+	fmt.Println(no1);
+
+	var no2 int32;
+
+	no2 = no1 - 3;
+	fmt.Println(no2);
+	no2++
+	fmt.Println(no2%2);
+}
