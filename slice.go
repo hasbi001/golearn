f410c1108ea525c7b0f2c2f0982516700fca8f4d
Hasbi Ali Firdaus <hasbialifirdaus001@gmail.com>
2023-01-05 23:47:05 +0700

    learn slice

--- slice.go
+++ slice.go
@@ -1,1 +1,26 @@
-slice
+package main
+
+import "fmt"
+
+func main(){
+	var monts = [...]string{
+		"januari",
+		"februari",
+		"maret",
+		"april",
+		"mei",
+		"juni",
+		"juli",
+		"agustus",
+		"september",
+		"oktober",
+		"november",
+		"desember",
+	}
+
+	var slice1 = monts[4:7]
+	fmt.Println(monts)
+	fmt.Println(slice1)
+	fmt.Println(len(slice1))
+	fmt.Println(cap(slice1))
+}
